ab576e6cd08e7e1e32e291e797f7bb90279cb84f
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2014-10-25 21:35:51 +0200

    Remove old tests from baselines

--- test/baselines/test-2/dts/test-2.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="../../../../../../../users/ivogabe/documents/websites/gulp-typescript-incremental/test/test-2/referenced.d.ts" />
--- test/baselines/test-4/js/test-4.js
+++
@@ -1,3 +0,0 @@
-asdf;
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGVzdC00LmpzIiwic291cmNlUm9vdCI6Ii4uLy4uLy4uL3Rlc3QtNC8iLCJzb3VyY2VzIjpbInRlc3QtNC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxJQUFJLENBQUMifQ==
