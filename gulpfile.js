a97c2f6d6032bc935f56e1efa383c670fae792cb
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-03-07 11:18:02 +0100

    Improve testing & type checking

--- gulpfile.js
+++ gulpfile.js
@@ -10,11 +10,13 @@ var concat = require('gulp-concat');
 var header = require('gulp-header');
 var diff = require('gulp-diff');
 
-var tsProject = ts.createProject({
+var tsOptions = {
 	target: 'es5',
 	module: 'commonjs',
-	noExternalResolve: true
-});
+	noExternalResolve: true,
+	preserveConstEnums: true
+};
+var tsProject = ts.createProject(tsOptions);
 
 var paths = {
 	scripts: ['lib/**.ts', 'definitions/**.ts'],
@@ -47,40 +49,57 @@ gulp.task('scripts', ['clean'], function() {
 		.pipe(gulp.dest(paths.releaseBeta));
 });
 
+// Check against the master of TypeScript
+gulp.task('typecheck-dev', function() {
+	return gulp.src(paths.scripts.concat([
+		'!defintions/typescript.d.ts',
+		path.resolve(require.resolve('typescript-dev'), './typescript.d.ts')
+	])).pipe(ts(tsOptions));
+});
+
+// Checking against the current release of TypeScript on NPM can be done using `gulp scripts`.
+gulp.task('typecheck', ['typecheck-dev']);
+
 function runTest(name, callback) {
 	var newTS = require('./release-2/main');
 	var libs = [
 		['default', undefined],
 		['ts-next', require('typescript-dev')],
-		['jsx', require('jsx-typescript')] // TODO: Add jsx-typescript here. It currently throws an error when adding it.
+		// ['jsx', require('jsx-typescript')] // TODO: Add jsx-typescript here. It currently throws an error when adding it.
 	];
 	var test = require('./test/' + name + '/gulptask.js');
 
+	fs.mkdirSync('test/output/' + name);
 	for (var i = 0; i < libs.length; i++) {
-		var lib = libs[i];
-		var output = 'test/output/' + name + '/' + lib[0] + '/';
-		var errors = [];
-		var reporter = {
-			error: function(err) {
-				errors.push(err);
-			}
-		};
-		test(newTS, lib[1], output, reporter).on('finish', function() {
-			fs.writeFileSync(output + 'errors.txt', errors);
-			function onError(error) {
-				console.error('Test ' + name + ' failed: ' + error.message);
-			}
-			gulp.src('test/output/' + name + '/**')
-				.pipe(diff('test/baselines/' + name))
-				.on('error', onError)
-				.pipe(diff.reporter({ fail: true }))
-				.on('error', onError)
-				.on('finish', callback);
-		});
+		(function(i) {
+			var lib = libs[i];
+			var output = 'test/output/' + name + '/' + lib[0] + '/';
+			var errors = [];
+			var reporter = {
+				error: function(err) {
+					errors.push(err);
+				}
+			};
+			fs.mkdirSync(output);
+			test(newTS, lib[1], output, reporter).on('finish', function() {
+				fs.writeFileSync(output + 'errors.txt', errors);
+				function onError(error) {
+					console.error('Test ' + name + ' failed: ' + error.message);
+				}
+				gulp.src('test/output/' + name + '/**')
+					.pipe(diff('test/baselines/' + name))
+					.on('error', onError)
+					.pipe(diff.reporter({ fail: true }))
+					.on('error', onError)
+					.on('finish', callback);
+			});
+		})(i);
 	}
 }
 
 gulp.task('test', ['clean-test', 'scripts'], function(cb) {
+	fs.mkdirSync('test/output/');
+
 	var currentTests = tests;
 	if (argv.tests !== undefined) {
 		currentTests = argv.tests.split(',');
