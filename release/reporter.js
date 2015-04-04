89ccd75fc2a6fd8acd08d84a3a02f51b7a924c81
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-04-04 11:06:29 +0200

    Update release

--- release/reporter.js
+++ release/reporter.js
@@ -1,6 +1,4 @@
 ///<reference path='../definitions/ref.d.ts'/>
-var ts = require('typescript');
-var tsApi = require('./tsapi');
 var gutil = require('gulp-util');
 function nullReporter() {
     return {};
@@ -14,22 +12,59 @@ function defaultReporter() {
     };
 }
 exports.defaultReporter = defaultReporter;
+function flattenDiagnosticsVerbose(message, index) {
+    if (index === void 0) { index = 0; }
+    if (typeof message === 'undefined') {
+        return '';
+    }
+    else if (typeof message === 'string') {
+        return message;
+    }
+    else {
+        var result;
+        if (index === 0) {
+            result = message.messageText;
+        }
+        else {
+            result = '\n> TS' + message.code + ' ' + message.messageText;
+        }
+        return result + flattenDiagnosticsVerbose(message.next, index + 1);
+    }
+}
+function longReporter() {
+    return {
+        error: function (error) {
+            if (error.tsFile) {
+                console.error('[' + gutil.colors.gray('gulp-typescript') + '] ' + gutil.colors.red(error.fullFilename + '(' + error.startPosition.line + ',' + error.startPosition.character + '): ') + 'error TS' + error.diagnostic.code + ' ' + flattenDiagnosticsVerbose(error.diagnostic.messageText));
+            }
+            else {
+                console.error(error.message);
+            }
+        }
+    };
+}
+exports.longReporter = longReporter;
 function fullReporter(fullFilename) {
     if (fullFilename === void 0) { fullFilename = false; }
     return {
         error: function (error, typescript) {
-            console.error('[' + gutil.colors.gray('gulp-typescript') + '] ' + gutil.colors.bgRed(error.diagnostic.code + '') + ' ' + gutil.colors.red(tsApi.flattenDiagnosticMessageText(ts, error.diagnostic.messageText)));
+            console.error('[' + gutil.colors.gray('gulp-typescript') + '] '
+                + gutil.colors.bgRed(error.diagnostic.code + '')
+                + ' ' + gutil.colors.red(flattenDiagnosticsVerbose(error.diagnostic.messageText)));
             if (error.tsFile) {
                 console.error('> ' + gutil.colors.gray('file: ') + (fullFilename ? error.fullFilename : error.relativeFilename) + gutil.colors.gray(':'));
                 var lines = error.tsFile.text.split(/(\r\n|\r|\n)/);
                 var logLine = function (lineIndex, errorStart, errorEnd) {
-                    var line = lines[lineIndex - 1];
+                    var line = lines[lineIndex];
                     if (errorEnd === undefined)
                         errorEnd = line.length;
-                    console.error('> ' + gutil.colors.gray('[' + lineIndex + '] ') + line.substring(0, errorStart - 1) + gutil.colors.red(line.substring(errorStart - 1, errorEnd)) + line.substring(errorEnd));
+                    console.error('> ' + gutil.colors.gray('[' + lineIndex + '] ')
+                        + line.substring(0, errorStart)
+                        + gutil.colors.red(line.substring(errorStart, errorEnd))
+                        + line.substring(errorEnd));
                 };
                 for (var i = error.startPosition.line; i <= error.endPosition.line; i++) {
-                    logLine(i, i === error.startPosition.line ? error.startPosition.character : 0, i === error.endPosition.line ? error.endPosition.character : undefined);
+                    logLine(i, i === error.startPosition.line ? error.startPosition.character - 1 : 0, i === error.endPosition.line ? error.endPosition.character - 1 : undefined);
                 }
             }
         }
