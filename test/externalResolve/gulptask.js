c2bd94077ee940e91dc1552dd8241c93cbb924a6
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-04-24 09:53:08 +0200

    Fix gulptasks

--- test/externalResolve/gulptask.js
+++ test/externalResolve/gulptask.js
@@ -3,16 +3,16 @@ var sourcemaps = require('gulp-sourcemaps');
 
 module.exports = function(newTS, lib, output, reporter) {
 	var tsResult = gulp.src('test/externalResolve/test-2.ts')
-					   .pipe(sourcemaps.init())
-					   .pipe(newTS({
-							declarationFiles: true,
-							module: 'commonjs',
-							sourceRoot: '',
-						    typescript: lib
-					   }, undefined, reporter));
+		.pipe(sourcemaps.init())
+		.pipe(newTS({
+			declarationFiles: true,
+			module: 'commonjs',
+			sourceRoot: '',
+			typescript: lib
+		}, undefined, reporter));
 
 	tsResult.dts.pipe(gulp.dest(output + 'dts'));
 	return tsResult.js
-			.pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../../../../externalResolve/' }))
-			.pipe(gulp.dest(output + 'js'));
+		.pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../../../../externalResolve/' }))
+		.pipe(gulp.dest(output + 'js'));
 }
