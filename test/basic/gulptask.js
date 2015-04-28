5515bc82cfe1243aa165ba6d46826f1bfee10982
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-04-28 20:40:03 +0200

    Change test to use outDir

--- test/basic/gulptask.js
+++ test/basic/gulptask.js
@@ -9,12 +9,13 @@ module.exports = function(newTS, lib, output, reporter) {
 			module: 'amd',
 			noExternalResolve: true,
 			sourceRoot: '',
-			typescript: lib
+			typescript: lib,
+			outDir: output + 'js'
 		}, undefined, reporter));
 
 	// tsResult.map.pipe(gulp.dest('test/output/test-3/map'));
 	tsResult.dts.pipe(gulp.dest(output + '/dts'));
 	return tsResult.js
 		.pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: '../../../../basic/' }))
-		.pipe(gulp.dest(output + '/js'));
+		.pipe(gulp.dest(output + 'js'));
 }
