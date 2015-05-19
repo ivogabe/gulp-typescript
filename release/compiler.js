2e38de59517f1a61e77361b99552bcb23727f857
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-05-19 14:37:38 +0200

    Update LKG

--- release/compiler.js
+++ release/compiler.js
@@ -73,12 +73,17 @@ var ProjectCompiler = (function () {
             var expected = this.project.input.commonBasePath;
             var real = this.project.input.commonSourceDirectory;
             var length = real.length - expected.length;
+            this._commonBaseDiff = [length, real.substring(real.length - length)];
             if (length > 0) {
-                return this._commonBaseDiff = [length, real.substring(real.length - length)];
+                this._commonBaseDiff = [length, real.substring(real.length - length)];
             }
             else {
-                return this._commonBaseDiff = [length, expected.substring(expected.length + length)];
+                this._commonBaseDiff = [length, expected.substring(expected.length + length)];
             }
+            if (this._commonBaseDiff[1] === '/' || this._commonBaseDiff[1] === '\\') {
+                this._commonBaseDiff = [0, ''];
+            }
+            return this._commonBaseDiff;
         },
         enumerable: true,
         configurable: true
