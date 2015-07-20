094b974fcc8386b1149e9f61b61084cfba1c4289
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2015-07-20 20:20:10 +0200

    Update tests

---
+++ test/baselines/errorReporting/dev/js/test-4.js
@@ -0,0 +1,8 @@
+// Simple error
+asdf;
+// Nested errors
+var x = [[{ x: 0 }]];
+var y = [[{ y: "" }]];
+x = y;
+
+//# sourceMappingURL=test-4.js.map
---
+++ test/baselines/externalResolve/dev/dts/test-2.d.ts
@@ -0,0 +1,1 @@
+/// <reference path="referenced.d.ts" />
