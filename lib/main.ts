785b4d2742a6b3d80cf6afa39caf886d1b87ccec
Tim Lancina <tim@ionic.io>
2016-02-29 18:15:22 -0600

    Don't provide default target or module type
    Addresses https://github.com/ivogabe/gulp-typescript/issues/296.

--- lib/main.ts
+++ lib/main.ts
@@ -188,17 +188,6 @@ function getCompilerOptions(settings: compile.Settings): ts.CompilerOptions {
 		tsSettings['moduleResolution'] = <number> settings.moduleResolution;
 	}
 
-	if (tsSettings.target === undefined) {
-		// TS 1.4 has a bug that the target needs to be set.
-		// This block can be removed when a version that solves this bug is published.
-		// The bug is already fixed in the master of TypeScript
-		tsSettings.target = ts.ScriptTarget.ES3;
-	}
-	if (tsSettings.module === undefined) {
-		// Same bug in TS 1.4 as previous comment.
-		tsSettings.module = ts.ModuleKind.None;
-	}
-
 	if (settings.sourceRoot !== undefined) {
 		console.warn('gulp-typescript: sourceRoot isn\'t supported any more. Use sourceRoot option of gulp-sourcemaps instead.')
 	}
