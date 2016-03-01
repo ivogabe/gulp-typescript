0bd43bd34805764c80d3a7bb147b219afed74991
jwbay <justin.bay@gmail.com>
2016-03-01 01:33:33 -0500

    fixes #255 - don't change filename casing when adjusting source map sources

---
+++ test/sourceMaps/gulptask.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+
+module.exports = function(newTS, lib, output, reporter) {
+	var project = newTS.createProject('test/sourceMaps/Main/tsconfig.json', {
+		typescript: lib
+	});
+
+	return project.src()
+		.pipe(sourcemaps.init())
+		.pipe(newTS(project, undefined, reporter)).js
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(output + "js/Main"));
+}
