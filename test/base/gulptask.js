d0ce54321d5b56d158003fe8dbd978be78ddaa97
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2016-09-21 17:49:49 +0200

    Add test for tsProject.src() base path calculation
    See #253, regression of 98eca4ab34cb02e54589e40e9871cda59dce36cb

---
+++ test/base/gulptask.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+
+module.exports = function(newTS, lib, output, reporter) {
+	var tsProject = newTS.createProject('test/base/tsconfig.json', {
+		typescript: lib
+	});
+	
+	var tsResult = tsProject.src()
+		.pipe(newTS(tsProject, undefined, reporter));
+
+	tsResult.dts.pipe(gulp.dest(output + '/dts'));
+	return tsResult.js
+		.pipe(gulp.dest(output + 'js'));
+}
