9b91c8d2a717c33ac4483d54771169eacaa94fe4
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2016-12-12 11:29:26 +0100

    Add source maps test using outDir (#460)

---
+++ test/sourceMapsOutDir/gulptask.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+
+module.exports = function(newTS, lib, output, reporter) {
+	var project = newTS.createProject('test/sourceMapsOutDir/src/tsconfig.json', {
+		typescript: lib
+	});
+
+	return project.src()
+		.pipe(sourcemaps.init())
+		.pipe(project(reporter)).js
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest(output + "js/dist"));
+}
