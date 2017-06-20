f2d57a2d74afe539b0174093d113f4e1e8f83208
Sam Vervaeck <sam@vervaeck.be>
2017-06-20 21:23:01 +0200

    Fix compiler warning about unknown option 'cwd'

--- lib/main.ts
+++ lib/main.ts
@@ -177,8 +177,10 @@ module compile {
 				settings = fileNameOrSettings;
 			}
 		}
-
-		const project = _project.setupProject(projectDirectory, tsConfigContent, getCompilerOptions(settings, projectDirectory, tsConfigFileName), getTypeScript(settings.typescript), settings.cwd);
+    
+    const cwd = settings.cwd
+    delete settings.cwd
+		const project = _project.setupProject(projectDirectory, tsConfigContent, getCompilerOptions(settings, projectDirectory, tsConfigFileName), getTypeScript(settings.typescript), cwd);
 
 		return project;
 	}
