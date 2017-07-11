b552595abb4c168dcfb9598a8c93f1d8f4bb37f6
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2017-07-12 00:12:54 +0200

    Add test case with `files` and `include` (#525)

---
+++ test/tsconfigInclude/gulptask.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+
+module.exports = function(newTS, lib, output, reporter) {
+	var tsProject = newTS.createProject('test/tsconfigInclude/tsconfig.json', {
+		typescript: lib,
+	});
+
+	var tsResult = tsProject.src()
+		.pipe(tsProject(reporter));
+
+	return tsResult.pipe(gulp.dest(output));
+}
