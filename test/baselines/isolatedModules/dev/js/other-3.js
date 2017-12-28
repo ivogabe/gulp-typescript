2e31d567aaba07aa083a02ffcfaaee86c030255e
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2017-12-28 12:50:25 +0100

    Update baselines

--- test/baselines/isolatedModules/dev/js/other-3.js
+++ test/baselines/isolatedModules/dev/js/other-3.js
@@ -1,7 +1,7 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
     exports.__esModule = true;
-    var Hello = (function () {
+    var Hello = /** @class */ (function () {
         function Hello() {
         }
         return Hello;
