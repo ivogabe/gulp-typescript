2c098305bcd8d18aba3905f45f7f2be860a98641
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2018-06-08 17:29:06 +0200

    Changes for latest gulp & source-map

--- lib/output.ts
+++ lib/output.ts
@@ -25,6 +25,9 @@ export class Output {
 	// .d.ts files
 	streamDts: stream.Readable;
 
+	// Number of pending IO operatrions
+	private pendingIO = 0;
+
 	writeJs(base: string, fileName: string, content: string, sourceMapContent: string, cwd: string, original: input.File) {
 		const file = new VinylFile({
 			path: fileName,
@@ -32,26 +35,40 @@ export class Output {
 			cwd,
 			base
 		});
-		const appliedSourceMap = this.applySourceMap(sourceMapContent, original, file);
-		if (appliedSourceMap) file.sourceMap = JSON.parse(appliedSourceMap);
-		this.streamFull.push(file);
-		this.streamJs.push(file);
+
+		this.pendingIO++;
+
+		this.applySourceMap(sourceMapContent, original, file).then(appliedSourceMap => {
+			if (appliedSourceMap) file.sourceMap = JSON.parse(appliedSourceMap);
+			this.streamFull.push(file);
+			this.streamJs.push(file);
+
+			this.pendingIO--;
+			this.mightFinish();
+		});
 	}
 
-	writeDts(base: string, fileName: string, content: string, declarationMapContent: string, cwd: string, original: input.File) {
+	async writeDts(base: string, fileName: string, content: string, declarationMapContent: string, cwd: string, original: input.File) {
 		const file = new VinylFile({
 			path: fileName,
 			contents: new Buffer(content),
 			cwd,
 			base
 		});
-		const appliedSourceMap = this.applySourceMap(declarationMapContent, original, file);
-		if (appliedSourceMap) file.sourceMap = JSON.parse(appliedSourceMap);
-		this.streamFull.push(file);
-		this.streamDts.push(file);
+
+		this.pendingIO++;
+
+		this.applySourceMap(declarationMapContent, original, file).then(appliedSourceMap => {
+			if (appliedSourceMap) file.sourceMap = JSON.parse(appliedSourceMap);
+			this.streamFull.push(file);
+			this.streamDts.push(file);
+
+			this.pendingIO--;
+			this.mightFinish();
+		});
 	}
 
-	private applySourceMap(sourceMapContent: string, original: input.File, output: VinylFile) {
+	private async applySourceMap(sourceMapContent: string, original: input.File, output: VinylFile) {
 		if (sourceMapContent === undefined) return undefined;
 
 		const map = JSON.parse(sourceMapContent);
@@ -64,7 +81,8 @@ export class Output {
 
 		delete map.sourceRoot;
 
-		const generator = sourceMap.SourceMapGenerator.fromSourceMap(new sourceMap.SourceMapConsumer(map));
+		const consumer = await new sourceMap.SourceMapConsumer(map);
+		const generator = sourceMap.SourceMapGenerator.fromSourceMap(consumer);
 
 		const sourceMapOrigins = this.project.singleOutput
 			? this.project.input.getFileNames(true).map(fName => this.project.input.getFile(fName))
@@ -80,8 +98,9 @@ export class Output {
 			// We should only apply the input mappings if the input mapping isn't empty,
 			// since `generator.applySourceMap` has a really bad performance on big inputs.
 			if (inputMap.mappings !== '') {
-				const consumer = new sourceMap.SourceMapConsumer(inputMap);
-				generator.applySourceMap(consumer);
+				const inputConsumer = await new sourceMap.SourceMapConsumer(inputMap);
+				generator.applySourceMap(inputConsumer);
+				inputConsumer.destroy();
 			}
 
 			if (!inputMap.sources || !inputMap.sourcesContent) continue;
@@ -91,6 +110,7 @@ export class Output {
 				generator.setSourceContent(utils.forwardSlashes(relative), inputMap.sourcesContent[i]);
 			}
 		}
+		consumer.destroy();
 		return generator.toString();
 
 		function relativeToOutput(fileName: string) {
@@ -101,7 +121,14 @@ export class Output {
 
 	finish(result: reporter.CompilationResult) {
 		this.result = result;
-		if (this.project.reporter.finish) this.project.reporter.finish(result);
+
+		this.mightFinish();
+	}
+	
+	private mightFinish() {
+		if (this.result === undefined || this.pendingIO !== 0) return;
+
+		if (this.project.reporter.finish) this.project.reporter.finish(this.result);
 
 		this.streamFull.emit('finish');
 		this.streamFull.push(null);
