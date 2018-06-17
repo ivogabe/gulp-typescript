53823ab56aeba6cec3df23978772399bafa25bc3
Ivo Gabe de Wolff <ivogabe@ivogabe.nl>
2018-06-17 21:44:35 +0200

    Accept baselines for project references

--- test/baselines/tsconfigProjectReferences/dev/dts/b.d.ts
+++ test/baselines/tsconfigProjectReferences/dev/dts/b.d.ts
@@ -1,4 +1,2 @@
-/* Input file C:/dev/gulp-typescript/test/tsconfigProjectReferences/a/a.d.ts was missing */
-
 declare const b = 1;
 //# sourceMappingURL=b.d.ts.map
--- test/baselines/tsconfigProjectReferences/dev/js/b.js
+++ test/baselines/tsconfigProjectReferences/dev/js/b.js
@@ -1,7 +1,3 @@
-var a = 1;
-
-//# sourceMappingURL=a.js.map
-
 var b = 1;
 
 //# sourceMappingURL=b.js.map
